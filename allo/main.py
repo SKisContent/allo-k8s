2fda407231812eea7aad0d78b661d141973acc52
SKisContent <sushil@a-sk.com>
2023-05-11 15:12:52 +0530

    Workers call a sleep service rather than go to sleep locally

--- allo/main.py
+++ allo/main.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import make_wsgi_app, Summary
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "<p>Hello, World!</p>"
+
+# route sleep that takes a float number of seconds as a parameter
+@app.route("/sleep/<float:seconds>")
+def sleep(seconds):
+    # sleep for the specified number of seconds
+    time.sleep(seconds)
+    return f"<p>Slept for {seconds} seconds</p>"
 
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
     '/metrics': make_wsgi_app()
--- allo/worker.py
+++ allo/worker.py
@@ -1,6 +1,8 @@
 import random
 import time
 import sys
+import os
+import requests
 
 def numbers(a,b):
     return(i for i in range(a,b))
@@ -8,17 +10,19 @@ def numbers(a,b):
 def linear_sum(a):
     return(sum(numbers(a[0],a[1])))
 
+# get environment value for endpoint
+SLEEP_SERVICE = os.environ.get('SLEEP_SERVICE', 'http://localhost:8080/sleep/')
 
-# this function loops forever and randomly sleeps for 0-10 seconds
+# this function loops forever and calls the sleep service
 def do_work(sleep_time, run_time):
     start_time = time.time()
     while True:
         current_time = time.time()
+        print("Running time: {}".format(current_time - start_time))
         if current_time - start_time >= run_time:
-            print("Sleeping for {} seconds".format(sleep_time))
-            time.sleep(run_time)
+            print("Sleeping for {} seconds".format(sleep_time))            
+            resp = requests.get(SLEEP_SERVICE + str(sleep_time))
             start_time = time.time()
-        time.sleep(sleep_time)
         n = random.randint(0, 2000)
         sum = linear_sum([0, n+1])
         # print(sum)
